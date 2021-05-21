b647f127cdf5943d19295e2f7d7f080b213ffc94
JaBarosin <jbbrsn11@gmail.com>
2021-05-21 10:28:21 -0400

    First commit for NodeApp

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Heyo from nodejs! Testing jenkins builds + cbctl scanning. Test no. 1.0 !!');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
