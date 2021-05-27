9b75f4aa7978ac290edec883b784e4f8b440b718
JaBarosin <jbbrsn11@gmail.com>
2021-05-26 21:39:25 -0400

    edited deploy script - rsa keys added so no logon pwd prompt

--- deploy-nodeapp.sh
+++ deploy-nodeapp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-USERNAME=jake
 HOST="192.168.6.44"
-ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOST} "microk8s.kubectl apply -f /opt/jake-repos/node-docker/NodeApp/config/deployment.yaml"
+ssh ${HOST} "microk8s.kubectl apply -f /opt/jake-repos/node-docker/NodeApp/config/deployment.yaml"
