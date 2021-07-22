4f0369626feb85aba32fc30d8db395377c448f5f
JaBarosin <46300705+JaBarosin@users.noreply.github.com>
2021-07-22 12:23:04 -0400

    docker build test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Heyo from nodejs! Testing jenkins builds + cbctl scanning. Test no. 1.1 !!');
+  res.send('Heyo from nodejs! Testing jenkins builds + cbctl scanning. Test no. 1.2 !!');
 });
 
 app.listen(PORT, HOST);
